b9cdf5db313166ab3eda0ea98edc6303dd9085d8
Tatyana Milanova <tanyta78@gmail.com>
2018-10-14 11:39:05 +0300

    SIS.Framework -  Added MvcContext

---
+++ Homeworks/SIS/SIS.Framework/MvcContext.cs
@@ -0,0 +1,21 @@
+﻿namespace SIS.Framework
+{
+    public class MvcContext
+    {
+        private static MvcContext Instance;
+
+        private MvcContext(){}
+
+        public static MvcContext Get => Instance ?? (Instance = new MvcContext());
+
+        public string AssemblyName { get; set; }
+
+        public string ControllersFolder { get; set; } = "Controllers";
+
+        public string ControllersSuffix { get; set; } = "Controller";
+
+        public string ViewsFolder { get; set; } = "Views";
+
+        public string ModelsFolder { get; set; } = "Models";
+    }
+}
