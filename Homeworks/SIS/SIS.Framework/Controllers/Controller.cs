06b2c726e4bbf61d37b98c30934c792275ca1068
Tatyana Milanova <tanyta78@gmail.com>
2018-10-14 12:14:19 +0300

    SIS.Framework - added Controller class and it's utilities

---
+++ Homeworks/SIS/SIS.Framework/Controllers/Controller.cs
@@ -0,0 +1,25 @@
+﻿namespace SIS.Framework.Controllers
+{
+    using ActionResults;
+    using HTTP.Requests.Contracts;
+    using System.Runtime.CompilerServices;
+    using Utilities;
+    using Views;
+
+    public abstract class Controller
+    {
+        public IHttpRequest Request { get; set; }
+
+        protected IViewable View([CallerMemberName] string caller = "")
+        {
+            var controllerName = ControllerUtilities.GetControllerName(this);
+
+            var fullyQualifiedName = ControllerUtilities.GetViewFullQualifiedName(controllerName, caller);
+
+            var view = new View(fullyQualifiedName);
+            return new ViewResult(view);
+        }
+
+        protected IRedirectable RedirectToAction(string redirectUrl) => new RedirectResult(redirectUrl);
+    }
+}
---
+++ Homeworks/SIS/SIS.Framework/Utilities/ControllerUtilities.cs
@@ -0,0 +1,15 @@
+﻿namespace SIS.Framework.Utilities
+{
+    using System;
+
+    public static class ControllerUtilities
+    {
+        public static string GetControllerName(object controller)
+        {
+            return controller.GetType().Name.Replace(MvcContext.Get.ControllersSuffix, String.Empty);
+        }
+
+        public static string GetViewFullQualifiedName(string controller, string action) =>
+            string.Format("{0}\\{1}\\{2}", MvcContext.Get.ViewsFolder, controller, action);
+    }
+}
