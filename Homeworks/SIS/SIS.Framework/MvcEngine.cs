0644358dd490c22a73e9607499d4065607f5a58e
Tatyana Milanova <tanyta78@gmail.com>
2018-10-15 16:56:22 +0300

    Implement MvcEngine class for set up and running the server.

---
+++ Homeworks/SIS/SIS.Framework/MvcEngine.cs
@@ -0,0 +1,52 @@
+﻿namespace SIS.Framework
+{
+    using System;
+    using System.Reflection;
+    using WebServer;
+
+    public class MvcEngine
+    {
+        public static void Run(Server server)
+        {
+            RegisterAssemblyName();
+            RegisterControllersData();
+            RegisterViewsData();
+            RegisterModelsData();
+
+            try
+            {
+                server.Run();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+              
+            }
+        }
+
+        private static void RegisterModelsData()
+        {
+            MvcContext.Get.ModelsFolder = "Models";
+
+        }
+
+        private static void RegisterViewsData()
+        {
+            MvcContext.Get.ViewsFolder = "Views";
+
+        }
+
+        private static void RegisterControllersData()
+        {
+            MvcContext.Get.ControllersFolder = "Controllers";
+            MvcContext.Get.ControllersSuffix = "Controller";
+
+        }
+
+        private static void RegisterAssemblyName()
+        {
+            MvcContext.Get.AssemblyName = Assembly.GetEntryAssembly().GetName().Name;
+            //  MvcContext.Get.AssemblyName = Assembly.GetExecutingAssembly().GetName().Name;
+        }
+    }
+}
