95c7843022ad52fcbcce2c18132340f7253f379a
Tatyana Milanova <tanyta78@gmail.com>
2018-10-18 15:10:12 +0300

    Create MvcFrameworkTests and add some view engine tests. Choose XUnit for tests.

---
+++ SIS/SIS.MvcFramework.Tests/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace SIS.MvcFramework.Tests
+{
+    public class Class1
+    {
+    }
+}
---
+++ SIS/SIS.MvcFramework.Tests/ViewEngine/ViewEngineTests.cs
@@ -0,0 +1,29 @@
+﻿namespace SIS.MvcFramework.Tests.ViewEngine
+{
+    using System.IO;
+    using MvcFramework.ViewEngine;
+    using Xunit;
+
+    public class ViewEngineTests
+    {
+        [Theory]
+        [InlineData("IfForForeach")]
+        [InlineData("ViewWithNoCode")]
+        [InlineData("WorkWithViewModel")]
+        public void RunTestViews(string testViewName)
+        {
+            // read view
+            var viewCode = File.ReadAllText($"TestViews/{testViewName}.html");
+
+            //read result
+            var expectedResult = File.ReadAllText($"TestViews/{testViewName}.Result.html");
+
+            //run view engine
+            IViewEngine viewEngine = new ViewEngine();
+           var engineResult = viewEngine.GetHtml(viewCode);
+
+            // compare result!= view engine result
+            Assert.Equal(expectedResult,engineResult);
+        }
+    }
+}
