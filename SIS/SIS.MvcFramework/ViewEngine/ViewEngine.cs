9b9d1d2e70f21f62c4ed9962989b7dc60c51891c
Tatyana Milanova <tanyta78@gmail.com>
2018-10-19 10:06:14 +0300

    Finished view engine. All tests passed.

--- SIS/SIS.MvcFramework/ViewEngine/ViewEngine.cs
+++ SIS/SIS.MvcFramework/ViewEngine/ViewEngine.cs
@@ -7,6 +7,8 @@
     using System.IO;
     using System.Linq;
     using System.Reflection;
+    using System.Text;
+    using System.Text.RegularExpressions;
 
     public class ViewEngine : IViewEngine
     {
@@ -25,15 +27,16 @@ using SIS.MvcFramework.ViewEngine;
 using " + typeof(T).Namespace + @";
 namespace MyAppViews
 {
-    public class " + viewTypeName + @": IView<" + typeof(T).FullName.Replace("+",".") + @">
+    public class " + viewTypeName + @": IView<" + typeof(T).FullName.Replace("+", ".") + @">
     {
-        public string GetHtml(" + typeof(T).FullName.Replace("+",".")  + @" model)
+        public string GetHtml(" + typeof(T).FullName.Replace("+", ".") + @" model)
         {
             StringBuilder html = new StringBuilder();
+            var Model = model;
 
             " + csharpMethodBody + @"
 
-            return html.ToString();
+            return html.ToString().TrimEnd();
         }
     }
 }
@@ -53,11 +56,20 @@ namespace MyAppViews
             var compilation = CSharpCompilation.Create(tempFileName)
                 .WithOptions(new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary))
                 .AddReferences(MetadataReference.CreateFromFile(typeof(object).GetTypeInfo().Assembly.Location))
+                .AddReferences(MetadataReference.CreateFromFile(Assembly.Load(new AssemblyName("mscorlib")).Location))
+                .AddReferences(MetadataReference.CreateFromFile(Assembly.Load(new AssemblyName("netstandard")).Location))
+                .AddReferences(MetadataReference.CreateFromFile(typeof(Enumerable).GetTypeInfo().Assembly.Location))
                 .AddReferences(MetadataReference.CreateFromFile(typeof(IView<>).GetTypeInfo().Assembly.Location))
                 .AddReferences(MetadataReference.CreateFromFile(typeof(IEnumerable<>).GetTypeInfo().Assembly.Location))
-                .AddReferences(MetadataReference.CreateFromFile(Assembly.Load(new AssemblyName("netstandard")).Location))
-                .AddReferences(MetadataReference.CreateFromFile(viewModelType.Assembly.Location))
-                .AddSyntaxTrees(CSharpSyntaxTree.ParseText(cSharpCode));
+                .AddReferences(MetadataReference.CreateFromFile(viewModelType.Assembly.Location));
+
+            var netStandardReferences = Assembly.Load(new AssemblyName("netstandard")).GetReferencedAssemblies();
+            foreach (var netStandardReference in netStandardReferences)
+            {
+                compilation = compilation.AddReferences(MetadataReference.CreateFromFile(Assembly.Load(netStandardReference).Location));
+            }
+
+            compilation = compilation.AddSyntaxTrees(CSharpSyntaxTree.ParseText(cSharpCode));
 
             using (var ms = new MemoryStream())
             {
@@ -77,20 +89,83 @@ namespace MyAppViews
                     return null;
                 }
 
-
                 ms.Seek(0, SeekOrigin.Begin);
                 var assembly = Assembly.Load(ms.ToArray());
                 var viewType = assembly.GetType(typeName);
                 return Activator.CreateInstance(viewType);
-
             }
-
-
         }
 
         private string GenerateCSharpMethodBody(string viewCode)
         {
-            return "";
+            var codeLines = this.GetLines(viewCode);
+            var sb = new StringBuilder();
+            foreach (var line in codeLines)
+            {
+                if (line.Trim().StartsWith("{") ||
+                    line.Trim().StartsWith("}") ||
+                    line.Trim().StartsWith("@for") ||
+                    line.Trim().StartsWith("@else") ||
+                    line.Trim().StartsWith("@if"))
+                {
+                    // CSharp
+                    var firstAtSymbolIndex = line.IndexOf("@", StringComparison.InvariantCulture);
+                    sb.AppendLine(this.RemoveAt(line, firstAtSymbolIndex));
+                }
+                else
+                {
+                    var htmlLine = line.Replace("\"", "\\\"");
+                    while (htmlLine.Contains("@"))
+                    {
+                        var specialSymbolIndex = htmlLine.IndexOf("@", StringComparison.InvariantCulture);
+                        var endOfCode = new Regex(@"[\s<\\]+").Match(htmlLine, specialSymbolIndex).Index;
+                        string expression = null;
+                        if (endOfCode == 0 || endOfCode == -1)
+                        {
+                            expression = htmlLine.Substring(specialSymbolIndex + 1);
+                            htmlLine = htmlLine.Substring(0, specialSymbolIndex) +
+                                       "\" + " + expression + " + \"";
+                        }
+                        else
+                        {
+                            expression = htmlLine.Substring(specialSymbolIndex + 1, endOfCode - specialSymbolIndex - 1);
+                            htmlLine = htmlLine.Substring(0, specialSymbolIndex) +
+                                       "\" + " + expression + " + \"" + htmlLine.Substring(endOfCode);
+                        }
+
+                    }
+
+                    sb.AppendLine($"html.AppendLine(\"{htmlLine}\");");
+                }
+
+            }
+
+            return sb.ToString();
+            
+        }
+
+        private IEnumerable<string> GetLines(string input)
+        {
+            var stringReader = new StringReader(input);
+            var lines = new List<string>();
+
+            string line = null;
+            while ((line = stringReader.ReadLine()) != null)
+            {
+                lines.Add(line);
+            }
+
+            return lines;
+        }
+
+        private string RemoveAt(string input, int index)
+        {
+            if (index == -1)
+            {
+                return input;
+            }
+
+            return input.Substring(0, index) + input.Substring(index + 1);
         }
     }
 }
