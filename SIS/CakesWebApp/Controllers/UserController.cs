7a468c588283b58c1a56debb771307b3c24010e6
Tanya Milanova <tanyta78@gmail.com>
2018-10-20 22:49:31 +0300

    Cakes - user controller  and viewModel added. Change connection string to support PC123.

---
+++ SIS/CakesWebApp/Controllers/UserController.cs
@@ -0,0 +1,29 @@
+﻿namespace CakesWebApp.Controllers
+{
+    using System.Linq;
+    using SIS.HTTP.Responses.Contracts;
+    using SIS.MvcFramework;
+    using ViewModels.User;
+
+    public class UserController : BaseController
+    {
+        [HttpGet("/user/profile")]
+        public IHttpResponse Profile()
+        {
+           var viewModel =  this.Db.Users.Where(x => x.Username == this.User)
+                .Select(x=>new ProfileViewModel
+                  {
+                      Username = x.Username,
+                      DateOfRegister = x.DateOfRegistration,
+                      OrdersCount = x.Orders.Count
+                  }).FirstOrDefault();
+
+            if (viewModel == null)
+            {
+                return this.BadRequestError("Profile page not accessible for this user.");
+            }
+
+            return this.View("Profile",viewModel);
+        }
+    }
+}
---
+++ SIS/CakesWebApp/ViewModels/User/ProfileViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace CakesWebApp.ViewModels.User
+{
+    using System;
+
+    public class ProfileViewModel
+    {
+        public string Username { get; set; }
+
+        public DateTime DateOfRegister { get; set; }
+
+        public int OrdersCount { get; set; }
+    }
+}
