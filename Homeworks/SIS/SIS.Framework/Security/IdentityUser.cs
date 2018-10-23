bc7ff566f4be412c83c4a52bad8cf70d57e85a57
Tanya Milanova <tanyta78@gmail.com>
2018-10-23 15:12:21 +0300

    SIS.Framework - start implement security functionality.

---
+++ Homeworks/SIS/SIS.Framework/Security/IdentityUser.cs
@@ -0,0 +1,12 @@
+﻿namespace SIS.Framework.Security
+{
+    using System;
+
+    public class IdentityUser : IIdentityUser<string>
+    {
+        public IdentityUser()
+        {
+            this.Id = Guid.NewGuid().ToString();
+        }
+    }
+}
