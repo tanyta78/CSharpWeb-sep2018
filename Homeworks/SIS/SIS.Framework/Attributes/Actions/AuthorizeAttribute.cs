491b43ec2b8262bc186442d64b2ed7e32ca4b1c1
Tanya Milanova <tanyta78@gmail.com>
2018-10-23 15:30:58 +0300

    SIS.Framework - Implement AuthorizeAttribute.

---
+++ Homeworks/SIS/SIS.Framework/Attributes/Actions/AuthorizeAttribute.cs
@@ -0,0 +1,48 @@
+﻿namespace SIS.Framework.Attributes.Actions
+{
+    using System;
+    using System.Linq;
+    using Security.Contracts;
+
+    public class AuthorizeAttribute : Attribute
+    {
+        private readonly string role;
+
+        public AuthorizeAttribute()
+        {
+
+        }
+
+        public AuthorizeAttribute(string role)
+        {
+            this.role = role;
+        }
+
+        private bool IsIdentityPresent(IIdentity identity)
+        {
+            return identity != null;
+        }
+
+        private bool IsIdentityInRole(IIdentity identity)
+        {
+            if (!this.IsIdentityPresent(identity))
+            {
+                return false;
+            }
+
+            return identity.Roles.Any(r => r == this.role);
+        }
+
+        private bool IsAuthorized(IIdentity user)
+        {
+            if (this.role == null)
+            {
+                return this.IsIdentityPresent(user);
+            }
+            else
+            {
+                return this.IsIdentityInRole(user);
+            }
+        }
+    }
+}
