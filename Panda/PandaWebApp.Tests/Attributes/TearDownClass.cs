841fdadfabcf27dbe28c6d0d784cff05272ad3bb
Tanya Milanova <tanyta78@gmail.com>
2018-12-05 13:22:01 +0200

    PandaWebApp- added test project

---
+++ Panda/PandaWebApp.Tests/Attributes/TearDownClass.cs
@@ -0,0 +1,59 @@
+﻿namespace PandaWebApp.Tests.Attributes
+{
+    using System;
+    using System.Configuration;
+    using System.IO;
+    using NUnit.Framework;
+    using NUnit.Framework.Interfaces;
+    using OpenQA.Selenium;
+    using Pages;
+
+    public class TearDownClass : BasePage
+    {
+        public TearDownClass(IWebDriver driver) : base(driver)
+        {
+        }
+
+        public void TearLogs()
+        {
+           // string path = AppDomain.CurrentDomain.BaseDirectory + "Logs\\";
+             string path = ConfigurationManager.AppSettings["Logs"];
+             path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, path);
+
+            string filename = path + TestContext.CurrentContext.Test.Name + ".txt";
+            string filenameJpg = path + TestContext.CurrentContext.Test.Name + ".jpg";
+            if (File.Exists(filename))
+            {
+                File.Delete(filename);
+            }
+
+            string testContextInfo = $"[ {DateTime.Now} ] [ {TestContext.CurrentContext.Result.Outcome.Status} ] [ {TestContext.CurrentContext.Test.MethodName} ] :: {TestContext.CurrentContext.Result.Message} ";
+
+            if (TestContext.CurrentContext.Result.Outcome.Status == TestStatus.Failed)
+            {
+                filename = path + TestContext.CurrentContext.Test.Name + "  [FAILED] " + ".txt";
+                ;
+                File.WriteAllText(filename, testContextInfo);
+
+            }
+            else
+            {
+                File.WriteAllText(filename, testContextInfo);
+            }
+
+
+            if (TestContext.CurrentContext.Result.Outcome.Status == TestStatus.Failed)
+            {
+                if (File.Exists(filenameJpg))
+                {
+                    File.Delete(filenameJpg);
+                }
+
+                filenameJpg = path + TestContext.CurrentContext.Test.Name + "  [FAILED] " + ".jpg";
+                var screenshot = ((ITakesScreenshot) this.Driver).GetScreenshot();
+                screenshot.SaveAsFile(filenameJpg, ScreenshotImageFormat.Jpeg);
+
+            }
+        }
+    }
+}
---
+++ Panda/PandaWebApp.Tests/Pages/LoginPage/LoginPageMap.cs
@@ -0,0 +1,55 @@
+﻿namespace PandaWebApp.Tests.Pages.LoginPage
+{
+    using OpenQA.Selenium;
+
+    public partial class LoginPage
+    {
+        public IWebElement LogInBtn => this.Driver.FindElement(By.XPath("/html/body/div/main/form/div[3]/button"));
+
+        public IWebElement LogInUsername => this.Driver.FindElement(By.Id("username"));
+
+        public IWebElement LogInPassword => this.Driver.FindElement(By.Id("password"));
+
+        
+        //public IWebElement LogOffBtn
+        //{
+        //    get
+        //    {
+        //        return this.Driver.FindElement(By.XPath("//*[@id=\"logoutForm\"]/ul/li[3]/a"));
+        //    }
+        //}
+        //public IWebElement ErrorMessageInvalidEmail
+        //{
+        //    get
+        //    {
+        //        this.Wait.Until(w => w.FindElement(By.XPath("/html/body/div[2]/div/div/form/div[1]/div/span/span")));
+        //        return this.Driver.FindElement(By.XPath("/html/body/div[2]/div/div/form/div[1]/div/span/span"));
+        //    }
+        //}
+        //public IWebElement ErrorMessageInvalidPassword
+        //{
+        //    get
+        //    {
+        //        this.Wait.Until(w => w.FindElement(By.XPath("/html/body/div[2]/div/div/form/div[1]/ul/li")));
+        //        return this.Driver.FindElement(By.XPath("/html/body/div[2]/div/div/form/div[1]/ul/li"));
+        //    }
+        //}
+        //public IWebElement ErrorMessageWithoutEmail
+        //{
+        //    get
+        //    {
+        //        this.Wait.Until(w => w.FindElement(By.XPath("html/body/div[2]/div/div/form/div[1]/div/span/span")));
+        //        return this.Driver.FindElement(By.XPath("html/body/div[2]/div/div/form/div[1]/div/span/span"));
+        //    }
+        //}
+        //public IWebElement ErrorMessageWithoutPassword
+        //{
+        //    get
+        //    {
+        //        this.Wait.Until(w => w.FindElement(By.XPath("/html/body/div[2]/div/div/form/div[2]/div/span/span")));
+        //        return this.Driver.FindElement(By.XPath("/html/body/div[2]/div/div/form/div[2]/div/span/span"));
+        //    }
+        //}
+    }
+}
+
